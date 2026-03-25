698f676133c88a40f2f98f9560726386d5ba35e1
Draconic-Hacker <pythontesteemail55@gmail.com>
2026-03-25 00:01:27 -0300

    Update command to start browser with correct path

--- install.ps1
+++ install.ps1
@@ -42,5 +42,5 @@ Read-Host | Out-Null
 
 # Chama o script pelo caminho completo, pois o PATH novo ainda não carregou nesta sessão
 # & $batFile
-Start-Process cmd -ArgumentList '/k navegador'
+Start-Process cmd -ArgumentList "/k cd $env:USERPROFILE\browser-files & navegador.bat"
 exit
