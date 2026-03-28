09aff682bb01af6f10df21cb8bb503e8ea3e0599
Draconic-Hacker <pythontesteemail55@gmail.com>
2026-03-27 23:49:33 -0300

    Enhance update.ps1 for browser-files updates
    Updated script to download and execute navegador.bat with improved messaging.

--- update.ps1
+++ update.ps1
@@ -1,8 +1,18 @@
-# update.ps1
+cd $env:TEMP
+
+taskkill /fi "windowtitle eq Meu Gerenciador" /f
+
+cls
+
 $installDir = Join-Path $HOME "browser-files"
 $batFile = Join-Path $installDir "navegador.bat"
-$url = "https://raw.githubusercontent.com/Draconic-Hacker/scripts-batch/refs/heads/master/navegador.bat"
+$urlMain = "https://raw.githubusercontent.com/Draconic-Hacker/scripts-batch/refs/heads/master/navegador.bat"
 
-Write-Host "Verificando atualizações para browser-files..." -ForegroundColor Cyan
-Invoke-WebRequest -Uri $url -OutFile $batFile -ErrorAction Stop
-Write-Host "Atualizado com sucesso!" -ForegroundColor Green ; timeout /t 2 >null ; pause ; browser-files
+Write-Host "Fazendo a atualizacao dos arquivos de Browser-Files..." -ForegroundColor Cyan
+write-host "`nAutalizacoes concluidas, pressione Enter para abrir." -Foregroundcolor Green
+Read-Host | Out-Null
+
+$navegadorContent = Invoke-WebRequest -Uri $urlMain -UseBasicParsing
+[System.IO.File]::WriteAllText($batFile, $navegadorContent.Content, [System.Text.Encoding]::ASCII)
+
+start cmd "/k cd $home/browser-files & navegador.bat"
